eb314961992f75609ee207e89c7b6092c85e760b
Hyruki <88951146+Hyruki@users.noreply.github.com>
2023-04-01 19:04:37 +0200

    Update enable senf money

--- money.py
+++ money.py
@@ -1,5 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import time
 import os
 
 scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
@@ -13,11 +14,35 @@ sheet = client.open("DataMoney").sheet1
 Quentin = int(sheet.cell(1,2).value)
 Jessy = int(sheet.cell(2,2).value)
 Enzo = int(sheet.cell(3,2).value)
+Evan = int(sheet.cell(4,2).value)
+Gio = int(sheet.cell(5,2).value)
+Vincent = int(sheet.cell(5,2).value)
 
+usergr1Q = 1
+usergr2Q = 2
+
+usergr1J = 2
+usergr2J = 2
+
+usergr1En = 3
+usergr2En = 2
+
+usergr1Ev = 4
+usergr2Ev = 2
+
+usergr1G = 5
+usergr2G = 2
+
+usergr1V = 6
+usergr2V = 2
+
+user_select = input("Qui est tu ? : ")
+
+isconnect = True
 
 #sheet.update_cell(1,2, str(Quentin + 1))
-
-print(""" __       __                                         
+def moneymain():
+    print(""" __       __                                         
 |  \     /  \                                        
 | $$\   /  $$  ______   _______    ______   __    __ 
 | $$$\ /  $$$ /      \ |       \  /      \ |  \  |  \
@@ -30,14 +55,16 @@ print(""" __       __
                                             \$$    $$
                                              \$$$$$$ """)
 
-reponse = input("""Que veux tu faire ? : \n
+    reponse = input("""Que veux tu faire ? : \n
 1. Gérer mon argent \n
 2. Donner de l'agent\n
-3. Mettre à jour mon argent\n
+3. Afficher le classement\n
 : """)
-     
-if reponse == "1":
-    print(""" __       __                                                   
+
+#==========================Money========================
+ 
+    if reponse == "1":
+        print(""" __       __                                                   
 |  \     /  \                                                  
 | $$\   /  $$  ______   _______    ______    ______    ______  
 | $$$\ /  $$$ |      \ |       \  |      \  /      \  /      \ 
@@ -50,12 +77,13 @@ if reponse == "1":
                                             \$$    $$          
                                              \$$$$$$           """)
 
-    getordeletemoney = input("""Voulez vous :\n
+        getordeletemoney = input("""Voulez vous :\n
     1. Mettre de l'argent\n
     2. Enlever de l'argent\n""")
                          
-    if getordeletemoney == "1":
-        num_get = input("""  ______   ________  ________ 
+        if getordeletemoney == "1":
+            num_get = input("""
+  ______   ________  ________ 
  /      \ |        \|        \
 |  $$$$$$\| $$$$$$$$ \$$$$$$$$
 | $$ __\$$| $$__       | $$   
@@ -69,9 +97,9 @@ if reponse == "1":
                               \n
 Combien voulez vous d'argent ?
 : """)
-        sheet.update_cell(1,2, str(Quentin+int(num_get)))
-    elif getordeletemoney == "2":
-        num_delete = input(""" _______                                                        
+            sheet.update_cell(usergr1, usergr2, str(user + int(num_get)))
+        elif getordeletemoney == "2":
+            num_delete = input(""" _______                                                        
 |       \                                                       
 | $$$$$$$\  ______   ______ ____    ______  __     __   ______  
 | $$__| $$ /      \ |      \    \  /      \|  \   /  \ /      \ 
@@ -85,4 +113,87 @@ Combien voulez vous d'argent ?
                                                                 \n
 Combien voulez vous enlever ?
 :""")
-        sheet.update_cell(1,2, str(Quentin - int(num_delete)))
+            sheet.update_cell(usergr1, usergr2, str(user - int(num_delete)))
+
+#====================SEND===================
+
+
+    elif reponse == "2":
+        user_givemoney = input("""  ______   __                      
+ /      \ |  \                     
+|  $$$$$$\ \$$ __     __   ______  
+| $$ __\$$|  \|  \   /  \ /      \ 
+| $$|    \| $$ \$$\ /  $$|  $$$$$$\
+| $$ \$$$$| $$  \$$\  $$ | $$    $$
+| $$__| $$| $$   \$$ $$  | $$$$$$$$
+ \$$    $$| $$    \$$$    \$$     \
+  \$$$$$$  \$$     \$      \$$$$$$$
+                                   
+                                   
+                                   \n
+Combien voulez vous donnez ? 
+: """)
+        user_getmoney = input("""  ______                             __ 
+ /      \                           |  \
+|  $$$$$$\  ______   _______    ____| $$
+| $$___\$$ /      \ |       \  /      $$
+ \$$    \ |  $$$$$$\| $$$$$$$\|  $$$$$$$
+ _\$$$$$$\| $$    $$| $$  | $$| $$  | $$
+|  \__| $$| $$$$$$$$| $$  | $$| $$__| $$
+ \$$    $$ \$$     \| $$  | $$ \$$    $$
+  \$$$$$$   \$$$$$$$ \$$   \$$  \$$$$$$$
+                                        
+                                        
+                                        \n
+A qui voulez vous l'envoyer ?
+:""")
+                           
+        if user_getmoney == "Jessy":
+            if user >= int(user_givemoney):
+                sheet.update_cell(usergr1J, usergr2J, str(int(user_givemoney) + Jessy))
+                sheet.update_cell(usergr1, usergr2, str(user - int(user_givemoney)))
+            else:
+                print("Vous n'avez pas assez d'argent !")
+                time.sleep(3)
+                moneymain()
+
+
+while isconnect:
+    if user_select == "admin":
+        user = Quentin
+        usergr1 = 1
+        usergr2 = 2
+        moneymain()
+        isconnect = False
+    elif user_select == "Jessy":
+        user = Jessy
+        usergr1 = 2
+        usergr2 = 2
+        moneymain()
+        isconnect = False
+    elif user_select == "Enzo":
+        user = Enzo
+        usergr1 = 3
+        usergr2 = 2
+        moneymain()
+        isconnect = False
+    elif user_select == "Evan":
+        user = Evan
+        usergr1 = 4
+        usergr2 = 2
+        moneymain()
+        isconnect = False
+    elif user_select == "Gio":
+        user = Gio
+        usergr1 = 5
+        usergr2 = 2
+        moneymain()
+        isconnect = False
+    elif user_select == "Vincent":
+        user = Vincent
+        usergr1 = 6
+        usergr2 = 2
+        moneymain()
+        isconnect = False
+    else:
+        print("Nom Invalide Réessaye \n\n")
